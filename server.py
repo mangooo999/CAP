970256e4ce5b176dccbf550faec572ca0f7d9a5f
Ahmed999 <94259503+mangooo999@users.noreply.github.com>
2023-12-23 23:13:02 +0200

    Update server.py

--- server.py
+++ server.py
@@ -18,3 +18,11 @@ def read_item(tmp, dust):
     return {"result": result}
 
 
+@app.get("/predictttt")
+async def predict(temp: int = Query(..., title="Temperature", description="Temperature value"),
+                  dust: int = Query(..., title="Dust", description="Dust value")):
+    result = f"Temperature: {temp}, Dust: {dust}"
+    print(result)
+    return {"result": result}
+
+

dbd33c0e26befa9d351eabf4a9c2f7819a582177
Ahmed999 <94259503+mangooo999@users.noreply.github.com>
2023-12-23 23:18:15 +0200

    Update server.py

--- server.py
+++ server.py
@@ -18,11 +18,8 @@ def read_item(tmp, dust):
     return {"result": result}
 
 
-@app.get("/predictttt")
-async def predict(temp: int = Query(..., title="Temperature", description="Temperature value"),
-                  dust: int = Query(..., title="Dust", description="Dust value")):
-    result = f"Temperature: {temp}, Dust: {dust}"
-    print(result)
-    return {"result": result}
-
+@app.get("/predicttttt")
+def read(tmp : int, dust : int):
+    # Use your model to make predictions
+    return {"result": 4, "tmp" : tmp, "dust" : dust }
 

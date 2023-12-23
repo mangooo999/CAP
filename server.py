ba0e3e5276cec81e5d8328d8eb7e1b784de112bd
Ahmed999 <94259503+mangooo999@users.noreply.github.com>
2023-12-23 21:49:05 +0200

    Add files via upload

---
+++ server.py
@@ -0,0 +1,20 @@
+import pickle
+from fastapi import FastAPI
+
+
+
+
+app = FastAPI()
+
+
+model_path = "model_pkl.pkl"
+with open(model_path, 'rb') as file:
+    model = pickle.load(file)
+
+@app.get("/predict")
+def read_item(tmp, dust):
+    # Use your model to make predictions
+    result = model.predict([(tmp, dust)])
+    return {"result": result}
+
+

28a19c594c0a6bf38112aef2d2f47cd671f0b131
Ahmed999 <94259503+mangooo999@users.noreply.github.com>
2023-12-24 01:29:38 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,18 +1,23 @@
 import pickle
 from fastapi import FastAPI
+import uvicorn
+
 
 
 app = FastAPI()
 
-model=pickle.load(open("model_pkl.pkl","rb"))
+
+model_path = "model.pkl"
+with open(model_path, 'rb') as file:
+    model = pickle.load(file)
 
 
 @app.get("/predict")
-def predict(tmp : int , dust : int):
-    feat  = (tmp, dust)
-    res = model.predict([feat])
-    return {res}
+def read_item():
+    # Use your model to make predictions
+    result = list(model.predict([[5, 6]]))
+    return {"result": result[0].item()}
 
-    
+
 
 
